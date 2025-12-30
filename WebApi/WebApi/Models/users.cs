2f2bd00f0a9d81a6bfd8d4d0b9620133fc5d64f8
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-30 23:40:30 +0530

    Created users class

---
+++ WebApi/WebApi/Models/users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Users
+    {
+        [Key]
+        public int userId { get; set; }
+
+        public required String name { get; set; } = "";
+        public required String address { get; set; } = "";
+
+    }
+}
