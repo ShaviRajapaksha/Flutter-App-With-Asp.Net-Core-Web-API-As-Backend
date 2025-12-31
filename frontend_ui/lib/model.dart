ddeb91276d0cb721c0eabe634391969db19d5510
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-31 20:21:36 +0530

    feat: Add User model with JSON serialization

---
+++ frontend_ui/lib/model.dart
@@ -0,0 +1,18 @@
+class User {
+  final int userId;
+  final String name;
+  final String address;
+
+  const User({required this.userId, required this.name, required this.address});
+
+  factory User.formJson(Map<String, dynamic> json) => User(
+        userId: json['userId'],
+        name: json['name'],
+        address: json['address'],
+      );
+  Map<String, dynamic> toJson() => {
+        'userId': userId,
+        'name': name,
+        'address': address,
+      };
+}
