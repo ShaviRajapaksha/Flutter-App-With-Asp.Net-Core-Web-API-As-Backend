5860526008f6154f3ee0f139fea87b2f8c6b2798
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-31 21:53:42 +0530

    feat: Implement ApiHandler class for user data retrieval

---
+++ frontend_ui/lib/api_handler.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'model.dart';
+
+class ApiHandler {
+  final String baseUrl = "http://localhost:5206/api/users";
+
+  Future<List<User>> getUserData() async {
+    List<User> data = [];
+
+    final uri = Uri.parse(baseUrl);
+    try {
+      final response = await http.get(
+        uri,
+        headers: {"Content-Type": "application/jsonl;charset=UTF-8"},
+      );
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body);
+        data = jsonData.map((json) => User.formJson(json)).toList();
+        return data;
+      } else {
+        return data;
+      }
+    } catch (e) {
+      return data;
+    }
+  }
+}
