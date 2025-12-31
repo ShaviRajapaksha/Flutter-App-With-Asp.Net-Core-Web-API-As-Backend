f53a1c3800bc662a70609a21d8db74c248679ab4
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-31 20:26:23 +0530

    feat: Add MainPage widget with basic app structure

---
+++ frontend_ui/lib/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MMainPageState();
+}
+
+class _MMainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterApi'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        
+      ),
+    );
+  }
+}
